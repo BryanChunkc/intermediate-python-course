d77f9ea32bc14640a970b08a0b53ab0184070bbf
unknown <chunkachung@gmail.com>
2021-06-17 16:08:30 +0800

    added 2 dice roller

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
+import random
+
 def main():
-  print('You rolled a die')
+  dicetotal = 0
+  dicerolls = 2
+  
+  for i in range(0,dicerolls):
+    roll = random.randint(1,6)
+    dicetotal += roll 
+
+  print(f'You rolled a {dicetotal}')
 
 if __name__== "__main__":
   main()
